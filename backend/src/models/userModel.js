4aa44a22ebdb703c4546cabf963560409f674206
reisenx <bloomrock555@gmail.com>
2024-11-24 15:00:39 +0700

    chore: prettier formatting

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-    username: {type: String, required: true, unique: true},
-    score: {type: Number, default: 0, required: true},
-    consecutiveWins: {type: Number, default: 0, required: true},
-    },
-    {timestamps: true}
+  {
+    username: { type: String, required: true, unique: true },
+    score: { type: Number, default: 0, required: true },
+    consecutiveWins: { type: Number, default: 0, required: true },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
